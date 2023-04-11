44eff834b0bd0b65a4d003ef5d254580d0e1a989
BagiliSalmaBegam <128033740+BagiliSalmaBegam@users.noreply.github.com>
2023-04-11 14:41:18 +0530

    Add files via upload

---
+++ IDE/codes/idenone.cpp
@@ -0,0 +1,24 @@
+#include<Arduino.h>
+int P,Q;
+int C,R;
+void assign(int C,int R)
+{
+digitalWrite(3,R);
+digitalWrite(2,C);
+}
+void setup()
+{
+pinMode(2,OUTPUT);
+pinMode(3,OUTPUT);
+pinMode(5,INPUT);
+pinMode(6,INPUT);
+}
+void loop()
+{
+P=digitalRead(5);
+Q=digitalRead(6);
+C=(!(P^Q));
+R=!(P^Q);
+assign(C,R);
+}
+
